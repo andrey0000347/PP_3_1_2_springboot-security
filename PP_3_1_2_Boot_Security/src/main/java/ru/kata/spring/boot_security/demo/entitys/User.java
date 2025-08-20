9d240642ca7f47cdd16dee3e0d9d2d24f2111692
andrey0000347 <alebedev02@gmail.com>
2025-08-20 14:55:58 +0300

    Update User.java

--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/entitys/User.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/entitys/User.java
@@ -24,7 +24,7 @@ public class User implements UserDetails {
     private String email;
 
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name ="user_id"),
@@ -141,3 +141,4 @@ public class User implements UserDetails {
         return true;
     }
 }
+
