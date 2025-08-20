167e5cb5780d936d899824a5a95366edb7455454
andrey0000347 <alebedev02@gmail.com>
2025-08-20 14:56:36 +0300

    Update UserRepository.java

--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("Select u from User u left join fetch u.roles where u.username=:username")
     Optional<User> findByUsername(String username);
 
 }
+
